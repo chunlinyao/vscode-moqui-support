3b3d983ff802a465e2a4c61b466d040a9f47ef49
Yao Chunlin <chunlinyao@gmail.com>
2022-03-05 22:00:17 +0800

    initial version

---
+++ README.md
@@ -0,0 +1,12 @@
+# vscode-moqui-support README
+
+This is a simple extension for Moqui development.
+
+## Features
+
+- Hightlight groovy source in `<script>` tag.  TBD
+- Auto complete service name in `<service-call>`. TBD 
+- Auto complete `transition-include`. TBD
+- Auto complete `section-include`. TBD
+- Auto complete `entity-find` entity name. TBD
+  
---
+++ src/extension.ts
@@ -0,0 +1,37 @@
+/*
+Copyright (c) 2022 Yao Chunlin
+vscode-moqui-support is licensed under Mulan PSL v2.
+You can use this software according to the terms and conditions of the Mulan PSL v2.
+You may obtain a copy of Mulan PSL v2 at:
+         http://license.coscl.org.cn/MulanPSL2
+THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+See the Mulan PSL v2 for more details.
+*/
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+	
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "vscode-moqui-support" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('vscode-moqui-support.helloWorld', () => {
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Hello World from vscode moqui support!');
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
